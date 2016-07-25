023989c569b594e4685d324727795070b6f230aa
praveen <praveen@localhost>
2016-07-24 18:42:17 -0700

    Adding to GIT

---
+++ HackerRank/src/HourGlassArray.java
@@ -0,0 +1,34 @@
+	import java.io.*;
+	import java.util.*;
+	import java.text.*;
+	import java.math.*;
+	import java.util.regex.*;
+
+	public class HourGlassArray {
+
+	    public static void main(String[] args) {
+	        Scanner in = new Scanner(System.in);
+	        int arr[][] = new int[3][3];
+	        for(int arr_i=0; arr_i < 3; arr_i++){
+	            for(int arr_j=0; arr_j < 3; arr_j++){
+	                arr[arr_i][arr_j] = in.nextInt();
+	            }
+	        }
+	        int sum_hourglasses[] = new int[16];
+	        int hg_sum=0;
+	        for (int i=0;i<3;i++){
+	        	for(int j=0;j<3;j++){
+	        		if((i!=1&& j!=0) || (i!=1&& j!=2) || (i==1 && j==1)){
+	        			System.out.print(arr[i][j]+"("+i+","+j+")"+"\t");
+	        			hg_sum=hg_sum+arr[i][j];
+	        		}
+	        	}
+	        	System.out.println();
+	        }
+	        System.out.println("hg_sum-"+hg_sum);
+	    }
+	}
+	
+	
+	
+	
